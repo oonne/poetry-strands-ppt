b537685d29e0c99a25a5afe143c205a7fdca8a13
JAY <JAY@oonne.com>
2025-06-12 14:27:17 +0800

    test: 测试依赖

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,7 @@ import Image from 'next/image';
 const Home = async () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav>
-        <ul>
-          <li>本地环境变量: {process.env.NEXT_PUBLIC_ENV_NAME}</li>
-        </ul>
-      </nav>
-
       <Image src="/poetry-strands-ppt/img/logo.png" alt="logo" width={180} height={180} priority />
-
-      <div className="font-zqk text-4xl">
-        <h1>自定义字体: Hello World</h1>
-      </div>
     </main>
   );
 };
